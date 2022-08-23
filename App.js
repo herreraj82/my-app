7aca557dd9a124cf6de7148e90c4553e5793f712
herreraj82 <herrera.j82@gmail.com>
2022-08-22 21:19:59 -0400

    initial commit

---
+++ App.js
@@ -0,0 +1,141 @@
+import {
+  View,
+  Text,
+  Button,
+  StatusBar,
+  TouchableWithoutFeedback,
+  TextInput,
+  Dimensions,
+} from "react-native";
+import * as FileSystem from "expo-file-system";
+import { useState } from "react";
+
+export default function App() {
+  const [sentences, setSentences] = useState(0);
+  const [currPage, setCurrPage] = useState(0);
+
+  const osaat = async () => {
+    let book_uri = FileSystem.documentDirectory + "sherlock.txt";
+
+    if (!(await FileSystem.getInfoAsync(book_uri)).exists) {
+      await FileSystem.downloadAsync(
+        "https://www.gutenberg.org/files/1661/1661-0.txt",
+        book_uri
+      );
+    }
+
+    const book = await FileSystem.readAsStringAsync(book_uri);
+
+    // console.log(book);
+
+    const sentences = book
+      .replace(/\r\n/gi, "¶")
+      .replace(/\.{3}/gi, "…")
+      .match(/[^\.\?!\:]+[\.\?!\:]\"?¶?/g);
+
+    setSentences(sentences);
+
+    let save2_uri = FileSystem.documentDirectory + "save3.txt";
+    // if (!(await FileSystem.getInfoAsync(save2_uri)).exists) {
+    FileSystem.writeAsStringAsync(save2_uri, "0");
+    // }
+
+    setCurrPage(await FileSystem.readAsStringAsync(save2_uri));
+  };
+
+  const handlePress = async (summand) => {
+    const newPage = Math.min(
+      Math.max(Number(currPage) + summand, 0),
+      sentences.length
+    );
+
+    setCurrPage(newPage);
+
+    await FileSystem.writeAsStringAsync(
+      FileSystem.documentDirectory + "save2.txt",
+      newPage.toString()
+    );
+  };
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black",
+      }}
+    >
+      {sentences.length > 0 && (
+        <TouchableWithoutFeedback
+          onPress={(e) =>
+            handlePress(
+              e.nativeEvent.locationX / Dimensions.get("window").width < 1 / 2
+                ? -1
+                : 1
+            )
+          }
+          touchSoundDisabled={true}
+        >
+          <View
+            style={{
+              flex: 1,
+              width: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+              paddingTop: StatusBar.currentHeight,
+            }}
+          >
+            <Text
+              style={{
+                color: "cornsilk",
+                fontSize: 28,
+                textAlign: "center",
+              }}
+            >
+              {sentences[currPage].replace("¶", "")}
+            </Text>
+            {sentences[currPage].includes("¶") && (
+              <Text
+                style={{
+                  color: "cornsilk",
+                  fontSize: 33,
+                  top: 50,
+                }}
+              >
+                ¶
+              </Text>
+            )}
+          </View>
+        </TouchableWithoutFeedback>
+      )}
+
+      <View style={{ width: "100%" }}>
+        {!sentences && <Button title="Sherlock Holmes" onPress={osaat} />}
+
+        {sentences.length > 0 && (
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "space-between",
+              alignItems: "center",
+              paddingBottom: 50,
+            }}
+          >
+            <Button title="<--" onPress={() => handlePress(-1)} />
+            <TextInput
+              style={{ color: "white" }}
+              placeholder={currPage.toString()}
+              placeholderTextColor="white"
+              keyboardType="number-pad"
+              onSubmitEditing={(e) => setCurrPage(e.nativeEvent.text)}
+            />
+            <Text style={{ color: "white" }}>/</Text>
+            <Text style={{ color: "white" }}>{sentences.length}</Text>
+            <Button title="-->" onPress={() => handlePress(1)} />
+          </View>
+        )}
+      </View>
+    </View>
+  );
+}

6478bf67198fdc279453b99f27fabded747c3d30
herreraj82 <herrera.j82@gmail.com>
2022-10-11 20:13:01 -0400

    fixed regex

--- python/main.py
+++ python/main.py
@@ -7,13 +7,16 @@ import json
 def convert_epub(epub_name):
     book = epub.read_epub(epub_name)
     html_arr = book.get_items_of_type(ebooklib.ITEM_DOCUMENT)
-    res_string = ''
+    res_arr = []
 
     for doc in html_arr:
-        res_string = res_string + BeautifulSoup(doc.get_content(),'html.parser').get_text()
+        content = BeautifulSoup(doc.get_content(),'html.parser')
+        if(content.h2): res_arr.append(content.h2.get_text())
+        for p in content.find_all('p'):
+            for sentence in re.findall("[^\.\?!]+[\.\?!][\"\”]?",p.get_text().replace('\n','').replace('/t','').replace('  ', ' ').replace('Mr.','Mr').replace('Mrs.','Mrs')):
+                res_arr.append(sentence)
 
-    split_string = re.split(r"[;.]\s*", res_string)
 
-    json_string = json.dumps(split_string)
+    json_string = json.dumps(res_arr)
 
     return json_string
